5fcebef9e5288f62707da2877ad85ae3dfb31dab
joostcalis <joost_calis@hotmail.com>
2016-03-04 18:47:43 +0100

    added second feed

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -11,11 +11,15 @@ class NewsController < ApplicationController
     require 'nokogiri'
     require 'open-uri'
       doc = Nokogiri::XML(open("http://mlb.mlb.com/partnerxml/gen/news/rss/mlb.xml"))
-      news = Hash.from_xml(doc.to_xml)
+      mlb_news = Hash.from_xml(doc.to_xml)
+
+      doc1 = Nokogiri::XML(open("http://www.baseballamerica.com/today/rss/majors.xml"))
+      baseball_america_news = Hash.from_xml(doc1.to_xml)
 
 
       render json: {
-        news: news
+        mlb_news: mlb_news,
+        baseball_america_news: baseball_america_news
       }
   end
 end
