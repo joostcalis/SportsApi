453b98c5fd9486ef5b9c283cc96e65039f3afbbf
joostcalis <joost_calis@hotmail.com>
2016-03-04 16:12:16 +0100

    set up for front end testing

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,21 +1,21 @@
 class NewsController < ApplicationController
   def index
-    # news = [{title: "hallo ik ben nieuws", link: "ik ben een link", description: "ik ben de omschrijving"}, {title: "hallo ik ben nieuws1", link: "ik ben een link1", description: "ik ben de omschrijving1"}, {title: "hallo ik ben nieuws2", link: "ik ben een link2", description: "ik ben de omschrijving2"}]
-    # render json: {
-    #   meta: {
-    #     count: 3,
-    #     page: 0
-    #   },
-    #   news: news
-    # }
-    require 'nokogiri'
-    require 'open-uri'
-      doc = Nokogiri::XML(open("http://mlb.mlb.com/partnerxml/gen/news/rss/mlb.xml"))
-      news = Hash.from_xml(doc.to_xml)
-
-
-      render json: {
-        news: news
-      }
+    news = [{title: "hallo ik ben nieuws", link: "ik ben een link", description: "ik ben de omschrijving"}, {title: "hallo ik ben nieuws1", link: "ik ben een link1", description: "ik ben de omschrijving1"}, {title: "hallo ik ben nieuws2", link: "ik ben een link2", description: "ik ben de omschrijving2"}]
+    render json: {
+      meta: {
+        count: 3,
+        page: 0
+      },
+      news: news
+    }
+    # require 'nokogiri'
+    # require 'open-uri'
+    #   doc = Nokogiri::XML(open("http://mlb.mlb.com/partnerxml/gen/news/rss/mlb.xml"))
+    #   news = Hash.from_xml(doc.to_xml)
+    #
+    #
+    #   render json: {
+    #     news: news
+    #   }
   end
 end
