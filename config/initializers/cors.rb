b27ef5c63016cab8a34bad076def65954e0235eb
joostcalis <joost_calis@hotmail.com>
2016-03-05 15:51:03 +0100

    added chiel hackman to corsfile

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 class Sports::Application
    config.middleware.insert_before 0,  "Rack::Cors", debug: Rails.env.development?, logger: (-> { Rails.logger }) do
      allow do
-       origins 'localhost:3000', '127.0.0.1:3000', 'scientist-rat-50038.bitballoon.com',
+       origins 'localhost:3000', '127.0.0.1:3000', 'scientist-rat-50038.bitballoon.com', 'www.chielhackman.nl'
          /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d)?\z/
 
        resource '/cors',
